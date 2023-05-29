e311b51f4f68cafbbb4555ce5bcd549af5ab4c53
Mohammad ali <57767232+alihash-creator@users.noreply.github.com>
2023-05-29 15:14:49 +0500

    Update index.py

--- index.py
+++ index.py
@@ -7,8 +7,8 @@ from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredicto
 from langchain.llms.openai import OpenAI
 
 # Define a simple Streamlit app
-st.title("Ask Llama")
-query = st.text_input("What would you like to ask? (source: data/paul_graham_essay.txt)", "")
+st.title("Ask me anything!")
+query = st.text_input("What would you like to ask? (source: some 2023 events)", "")
 
 # If the 'Submit' button is clicked
 if st.button("Submit"):
